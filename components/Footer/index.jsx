f0e83f75958adb136ce2298b596fcfc6c51f6a12
Nancy <nancyweibe@gmail.com>
2022-10-07 20:13:04 +0300

    Update: Socials

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import styles from "./Footer.module.scss"
 import Link from "next/link";
 import { Button, Container, Row, Col } from "react-bootstrap";
-import { IconFacebook, IconTwitter } from "../Icon"
+import { IconFacebook, IconTwitter, IconTiktok, IconYoutube, IconLinkedIn } from "../Icon"
 
 const links = [
   { name: "Terms and conditions", link: "terms" },
@@ -28,7 +28,9 @@ const Header = () => {
             <div className={styles.socials}>
               <span>Our Social Media:</span>
               <div className={styles.socialInner}>
-                <a href="https://www.linkedin.com/company/muunifi-com/" target="blank"><IconFacebook /></a>
+                <a href="https://www.youtube.com/channel/UCgK1E8yH8yqUPwTanz1p-vA" target="blank"><IconYoutube /></a>
+                <a href="https://www.linkedin.com/company/muunifi-com/" target="blank"><IconLinkedIn /></a>
+                <a href="https://www.tiktok.com/@muunifi?_t=8WJLV97Cv4V&_r=1" target="blank"><IconTiktok /></a>
                 <a href="https://twitter.com/MuunifiSocial" target="blank"><IconTwitter /></a>
               </div>
             </div>
